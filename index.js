848d6f4eacb9eab349e1e6ecb6a9a53ce9ed4215
Nikhil-bit21 <swaminikhil21@gmail.com>
2024-05-22 10:15:09 +0530

    added base route

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ const PORT = process.env.PORT || 3000;
 const userRoutes = require('./Routes/userRoutes');
 const PropertyRoutes = require('./Routes/propertyRouter');
 
+app.get('/', async (req,res)=>{
+    try {
+
+        res.status(200).json({ message: `Find all the endpoint details at https://github.com/Nikhil-bit21/Rentify-api` });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
 app.use('/user' , userRoutes);
 app.use('/property',PropertyRoutes);
 
