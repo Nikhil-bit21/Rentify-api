7f8ec839d20a9f95aaea98cc004bbaa27bf33345
Nikhil-bit21 <swaminikhil21@gmail.com>
2024-05-23 11:07:07 +0530

    updated base route and connected notion page to it

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const db = require('./db');
 require('dotenv').config();
+const axios = require('axios');
 
 const bodyParser = require('body-parser'); 
 app.use(bodyParser.json()); // req.body
@@ -10,15 +11,41 @@ const PORT = process.env.PORT || 3000;
 const userRoutes = require('./Routes/userRoutes');
 const PropertyRoutes = require('./Routes/propertyRouter');
 
-app.get('/', async (req,res)=>{
-    try {
+// app.get('/', async (req,res)=>{
+//     try {
+//         res.redirect('https://nikhilbit21.notion.site/Rentify-Application-3b693b17a9d249c2b1201b3a1e2e7342');
+//         // res.status(200).json({ message: `Find all the endpoint details at https://github.com/Nikhil-bit21/Rentify-api` });
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json({ error: 'Internal Server Error' });
+//     }
+// })
 
-        res.status(200).json({ message: `Find all the endpoint details at https://github.com/Nikhil-bit21/Rentify-api` });
+app.get('/', async (req, res) => {
+    try {
+        const url = 'https://nikhilbit21.notion.site/Rentify-Application-3b693b17a9d249c2b1201b3a1e2e7342';
+        
+        // Make a HEAD request to check if the page is available
+        const response = await axios.head(url);
+        
+        if (response.status === 200) {
+            // Page is available, redirect to it
+            res.redirect(url);
+        } else {
+            // Page is not available, send the JSON message
+            res.status(200).json({ message: `Find all the endpoint details at https://github.com/Nikhil-bit21/Rentify-api` });
+        }
     } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal Server Error' });
+        // if (err.response && err.response.status === 404) {
+            // Notion page not found, send the JSON message
+            res.status(200).json({ message: `Find all the endpoint details at https://github.com/Nikhil-bit21/Rentify-api` });
+        // } else {
+            // Some other error occurred
+            console.error(err);
+        //     res.status(500).json({ error: 'Internal Server Error' });
+        // }
     }
-})
+});
 
 app.use('/user' , userRoutes);
 app.use('/property',PropertyRoutes);
